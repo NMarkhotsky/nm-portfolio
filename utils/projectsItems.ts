68bfb2dc3285fd0b661e06c911953d1163a3b6d1
Nikolay <marhotsky@gmail.com>
2025-05-09 12:24:56 +0300

    upd

--- utils/projectsItems.ts
+++ utils/projectsItems.ts
@@ -1,9 +1,24 @@
 export const projectsItems = [
 	{
+		title: "Alvex Consulting Group",
+		description:
+			"Multi-page Landing Website. Developed with React to ensure fast performance and scalable architecture. The project features a fully responsive design, optimized for all devices.",
+		image: "/projects/alvex-min.png",
+		techStack: [
+			"HTML",
+			"CSS",
+			"SASS",
+			"JavaScript",
+			"React",
+			"Responsive Design",
+		],
+		liveLink: "https://alvex.consulting/",
+	},
+	{
 		title: "Barber Shop",
 		description:
 			"Barber Shop Landing Page - This is a landing page for a barbershop, designed to attract clients and showcase the shop’s services. The page is built using pure HTML and styled with SASS, which ensures easy style management and allows for quick design adjustments.",
-		image: "/projects/barbershop.png",
+		image: "/projects/barbershop-min.png",
 		techStack: ["HTML", "CSS", "SASS", "JavaScript", "Responsive Design"],
 		liveLink: "https://nmarkhotsky.github.io/barbershop/",
 		gitHubLink: "https://github.com/NMarkhotsky/barbershop",
@@ -12,7 +27,7 @@ export const projectsItems = [
 		title: "Your Pet",
 		description:
 			'Project "Your Pet" - is a web service created to help you find your beloved domestic companion, who will become your true soulmate. You can create your personal account, post advertisements for selling or adopting animals, or find a new friend for your family.',
-		image: "/projects/yourPet.png",
+		image: "/projects/yourPet-min.png",
 		techStack: [
 			"HTML",
 			"CSS-in-JS",
@@ -28,25 +43,10 @@ export const projectsItems = [
 			"https://github.com/NMarkhotsky/your-pet-project-backend",
 	},
 	{
-		title: "Alvex Consulting Group",
-		description:
-			"Multi-page Landing Website. Developed with React to ensure fast performance and scalable architecture. The project features a fully responsive design, optimized for all devices.",
-		image: "/projects/alvex.png",
-		techStack: [
-			"HTML",
-			"CSS",
-			"SASS",
-			"JavaScript",
-			"React",
-			"Responsive Design",
-		],
-		liveLink: "https://alvex.consulting/",
-	},
-	{
 		title: "Ethical Monogamy",
 		description:
 			"A concept-driven informational website that challenges traditional views on relationships. The project explores the idea of ethical monogamy and promotes intentional, transparent connection between partners. Designed with a focus on clarity and user engagement. The project features a fully responsive design, optimized for all devices.",
-		image: "/projects/ethicalmonogamy.png",
+		image: "/projects/ethicalmonogamy-min.png",
 		techStack: [
 			"HTML",
 			"CSS",
@@ -61,7 +61,7 @@ export const projectsItems = [
 		title: "Snake Game",
 		description:
 			"A classic Snake game built with React.js and styled using Styled Components, with state management handled by Redux Toolkit. The game allows players to control a snake that grows longer as it consumes food, aiming to achieve the highest score without colliding with itself or the game boundaries.",
-		image: "/projects/snakegame.png",
+		image: "/projects/snakegame-min.png",
 		techStack: [
 			"HTML",
 			"CSS-in-JS",
@@ -77,10 +77,25 @@ export const projectsItems = [
 			"snake-game-backend-beta.vercel.apphttps://github.com/NMarkhotsky/snake-game-backend",
 	},
 	{
+		title: "Sparkl - Smm Agency",
+		description:
+			"A multi-page landing website built with React, offering fast performance and a scalable structure. Designed to be fully responsive and optimized for a experience on all devices.",
+		image: "/projects/sparkl-min.png",
+		techStack: [
+			"HTML",
+			"CSS",
+			"SASS",
+			"JavaScript",
+			"React",
+			"Responsive Design",
+		],
+		liveLink: "https://sparkl-kyiv.com.ua/",
+	},
+	{
 		title: "Ecosolution - Green Energy",
 		description:
 			"This landing site built with React. The layout is fully responsive and adapts seamlessly across all screen sizes.",
-		image: "/projects/ecosolution.png",
+		image: "/projects/ecosolution-min.png",
 		techStack: [
 			"HTML",
 			"CSS-in-JS",
@@ -95,7 +110,7 @@ export const projectsItems = [
 		title: "Shop for gamers",
 		description:
 			"Best shop for buying gaming products. The core idea of the site is to provide a flexible online store framework that adapts smoothly to various screen sizes.",
-		image: "/projects/shopforgame.png",
+		image: "/projects/shopforgame-min.png",
 		techStack: ["HTML", "CSS", "JavaScript", "Responsive Design"],
 		liveLink: "https://nmarkhotsky.github.io/Shop-for-gamers/",
 		gitHubLink: "https://github.com/NMarkhotsky/Shop-for-gamers/",
